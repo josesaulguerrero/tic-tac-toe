ad8bf6da7c8226b6473e33950cdedb5f4fb14e74
Jose Saul Serrano <Jose Saul Serrano>
2021-12-10 15:02:07 -0500

    created the game context with the basic variables

---
+++ src/context/GameContext.js
@@ -0,0 +1,30 @@
+import { createContext, useState } from "react";
+
+export const GameContext = createContext();
+
+export const GameContextProvider = ({ children }) => {
+
+   const createCell = (index) => ({
+      index,
+      isMarked: false,
+      markedBy: null
+   });
+
+   const [player, setPlayer] = useState("X"); // by default, the first player is always the X.
+
+   const [gameMode, setGameMode] = useState(null); // the user will decide who he wants to play against.
+
+   const [gameBoard, setGameBoard] = useState([
+      createCell(0), createCell(1), createCell(2),
+      createCell(3), createCell(4), createCell(5),
+      createCell(6), createCell(7), createCell(8),
+   ]);
+
+
+
+   return (
+      <GameContext.Provider value={{}}>
+         {children}
+      </GameContext.Provider>
+   );
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from '@pages/App';
+import { GameContextProvider } from '@context/GameContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GameContextProvider>
+      <App />
+    </GameContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
