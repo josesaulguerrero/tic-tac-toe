108d80be78fb8abf60d327a29698c79091b6b121
Jose Saul Serrano <Jose Saul Serrano>
2021-12-11 11:11:58 -0500

    created the basic structure of the config window

--- src/components/ConfigWindow.js
+++ src/components/ConfigWindow.js
@@ -4,7 +4,15 @@ import "@styles/ConfigWindow.scss";
 export const ConfigWindow = () => {
    return (
       <section className="ConfigWindow">
-         hola soy jose
+         <form className="ConfigForm">
+            <p className="ConfigInfo">X Always goes first.</p>
+            <section className="ConfigOptions">
+               <p>Please choose a game mode</p>
+               <input type="radio" checked name="configOption" id="playerVSplayer" value="playerVSplayer" className="ConfigOption" />
+               <input type="radio" name="configOption" id="playerVSmachine" value="playerVSmachine" className="ConfigOption" />
+            </section>
+            <input type="submit" value="Start Game!" />
+         </form>
       </section>
    );
 };
