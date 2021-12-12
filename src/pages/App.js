2d09c808bc03e373a6a7a351956a14575b60b901
Jose Saul Serrano <Jose Saul Serrano>
2021-12-12 06:15:50 -0500

    the components are working

--- src/pages/App.js
+++ src/pages/App.js
@@ -8,12 +8,17 @@ import { ConfigWindow } from "@components/ConfigWindow";
 import "@styles/App.scss";
 
 export const App = () => {
-  const { gameConfig: { gameState } } = useContext(GameContext);
+  const { gameConfig: { gameState, gameMode } } = useContext(GameContext);
   return (
     <div className="App">
-      hola
       {
         gameState === "no_set_up" && <ConfigWindow />
+      }
+      {
+        (gameState === "playing" && gameMode === "playerVSplayer") && <p>player vs player</p>
+      }
+      {
+        (gameState === "playing" && gameMode === "playerVSmachine") && <p>player vs machine</p>
       }
     </div>
   );
